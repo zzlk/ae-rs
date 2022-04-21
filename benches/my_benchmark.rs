4c9ed4fe4611fb183cf5bf309714b60bffa3f913
zzlk <2418897+zzlk@users.noreply.github.com>
2022-04-21 12:54:15 -0700

    Benchmark Commit
    group                         base                                     new
    -----                         ----                                     ---
    bazoopy/Decode 64KB           1.00      7.7±0.17ms     8.1 MB/sec      1.00      7.7±0.17ms     8.1 MB/sec
    bazoopy/Decode random 64KB    1.00      7.8±0.10ms     8.0 MB/sec      1.05      8.2±0.10ms     7.7 MB/sec
    bazoopy/Decode random 8KB     1.00  1007.6±28.18µs     7.8 MB/sec      1.05  1054.3±13.05µs     7.4 MB/sec
    bazoopy/Decode zeroes 64KB    1.00  1361.0±1220.63µs    45.9 MB/sec    7.26      9.9±0.31ms     6.3 MB/sec
    bazoopy/Decode zeroes 8KB     1.00  1180.7±24.57µs     6.6 MB/sec      1.11  1316.0±110.20µs     5.9 MB/sec
    bazoopy/Encode 64KB           1.00      4.0±0.13ms    15.7 MB/sec      1.00      4.0±0.13ms    15.7 MB/sec
    bazoopy/Encode random 64KB    1.02      4.0±0.04ms    15.8 MB/sec      1.00      3.9±0.05ms    16.1 MB/sec
    bazoopy/Encode random 8KB     1.01    502.3±5.95µs    15.6 MB/sec      1.00    495.5±5.04µs    15.8 MB/sec
    bazoopy/Encode zeroes 64KB    1.01      3.0±0.16ms    20.8 MB/sec      1.00      3.0±0.11ms    20.9 MB/sec
    bazoopy/Encode zeroes 8KB     1.00    368.3±4.91µs    21.2 MB/sec      1.01    372.4±6.35µs    21.0 MB/sec

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,37 +3,45 @@ use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion, 
 use rand::Rng;
 use std::io::Cursor;
 
-fn bench_encode<const SIZE: usize>(b: &mut Bencher, src: &[usize]) {
+fn bench_encode<const SIZE: usize>(b: &mut Bencher, src: &[usize; SIZE]) {
     let mut output = Vec::new();
     output.reserve(SIZE * 8);
-    let mut encoder = Encoder::new(&mut output);
-    let src = black_box(&src[..]);
+    let mut src2 = [0usize; SIZE];
+    for (i, &x) in src.iter().enumerate() {
+        src2[i] = black_box(x);
+    }
 
     b.iter(move || {
-        for &x in src {
-            encoder.encode_next(x).unwrap();
-        }
-    });
-}
-
-fn bench_decode<const SIZE: usize>(b: &mut Bencher, src: &[usize]) {
-    let mut output = Vec::new();
-    output.reserve(SIZE * 8);
-    let src = &src[..];
-
-    {
         let mut encoder = Encoder::new(&mut output);
         for &x in src {
             encoder.encode_next(x).unwrap();
         }
+        encoder.encode_end().unwrap();
+        black_box(output[output.len() - 1]);
+    });
+}
+
+fn bench_decode<const SIZE: usize>(b: &mut Bencher, src: &[usize; SIZE]) {
+    let mut output = Vec::new();
+    output.reserve(SIZE * 8);
+    let mut src2 = [0usize; SIZE];
+    for (i, &x) in src.iter().enumerate() {
+        src2[i] = black_box(x);
     }
-    let output = black_box(output.clone());
-    let mut cursor = Cursor::new(&output);
-    let mut decoder = Decoder::new(&mut cursor).unwrap();
+
+    {
+        let mut encoder = Encoder::new(&mut output);
+        for x in src2 {
+            encoder.encode_next(x).unwrap();
+        }
+    }
     b.iter(move || {
+        let mut cursor = Cursor::new(&output);
+        let mut decoder = Decoder::new(&mut cursor).unwrap();
         for _ in 0..SIZE {
             decoder.decode_next().unwrap();
         }
+        black_box(decoder.decode_next().unwrap());
     });
 }
 
@@ -45,41 +53,39 @@ fn criterion_benchmark(c: &mut Criterion) {
     {
         const SIZE: usize = 64 * 1024;
         g.throughput(Throughput::Bytes(SIZE as u64));
-        let src: &mut [usize] = &mut [0usize; SIZE];
-        rng.fill(src);
+        let mut src = [0usize; SIZE];
         for i in 0..src.len() {
-            src[i] = src[i] % 256;
+            src[i] = rng.gen::<usize>() % 256;
         }
-        g.bench_function("Encode random 64KB", |b| bench_encode::<SIZE>(b, src));
-        g.bench_function("Decode random 64KB", |b| bench_decode::<SIZE>(b, src));
+        g.bench_function("Encode random 64KB", |b| bench_encode::<SIZE>(b, &src));
+        g.bench_function("Decode random 64KB", |b| bench_decode::<SIZE>(b, &src));
     }
 
     {
         const SIZE: usize = 8 * 1024;
         g.throughput(Throughput::Bytes(SIZE as u64));
-        let src: &mut [usize] = &mut [0usize; SIZE];
-        rng.fill(src);
+        let mut src = [0usize; SIZE];
         for i in 0..src.len() {
-            src[i] = src[i] % 256;
+            src[i] = rng.gen::<usize>() % 256;
         }
-        g.bench_function("Encode random 8KB", |b| bench_encode::<SIZE>(b, src));
-        g.bench_function("Decode random 8KB", |b| bench_decode::<SIZE>(b, src));
+        g.bench_function("Encode random 8KB", |b| bench_encode::<SIZE>(b, &src));
+        g.bench_function("Decode random 8KB", |b| bench_decode::<SIZE>(b, &src));
     }
 
     {
         const SIZE: usize = 64 * 1024;
         g.throughput(Throughput::Bytes(SIZE as u64));
-        let src: &mut [usize] = &mut [0usize; SIZE];
-        g.bench_function("Encode zeroes 64KB", |b| bench_encode::<SIZE>(b, src));
-        g.bench_function("Decode zeroes 64KB", |b| bench_decode::<SIZE>(b, src));
+        let src = [0usize; SIZE];
+        g.bench_function("Encode zeroes 64KB", |b| bench_encode::<SIZE>(b, &src));
+        g.bench_function("Decode zeroes 64KB", |b| bench_decode::<SIZE>(b, &src));
     }
 
     {
         const SIZE: usize = 8 * 1024;
         g.throughput(Throughput::Bytes(SIZE as u64));
-        let src: &mut [usize] = &mut [0usize; SIZE];
-        g.bench_function("Encode zeroes 8KB", |b| bench_encode::<SIZE>(b, src));
-        g.bench_function("Decode zeroes 8KB", |b| bench_decode::<SIZE>(b, src));
+        let src = [0usize; SIZE];
+        g.bench_function("Encode zeroes 8KB", |b| bench_encode::<SIZE>(b, &src));
+        g.bench_function("Decode zeroes 8KB", |b| bench_decode::<SIZE>(b, &src));
     }
     g.finish();
 }
