205e7997bfcf9ac580d82e994a77378e539eba8a
zzlk <2418897+zzlk@users.noreply.github.com>
2022-04-21 10:58:37 -0700

    commit benchmark script

--- bench.sh
+++ bench.sh
@@ -1,2 +1,7 @@
 cargo bench
-critcmp base.json new
+echo "Benchmark Commit" >/tmp/txt
+echo "" >>/tmp/txt
+critcmp base.json new >> /tmp/txt
+critcmp --export base > base.json
+git add base.json
+git commit --allow-empty -F /tmp/txt
